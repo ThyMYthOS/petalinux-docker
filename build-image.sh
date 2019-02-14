94c5a35f89b90399f5b0b0fc7247caff6bb489df
Manuel Stahl <thymythos@googlemail.com>
2019-02-14 18:09:11 +0100

    Improve build-image.sh script
    The python http.server forks again into background so the $server_pid
    might be useless in some cases.

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-installer_dir=$1
+installer_dir=${1:-`pwd`}
 docker_context=`pwd`
 
 echo "Start to build petalinux tools docker image ..."
@@ -7,7 +7,6 @@ echo "-----------------------------------------------"
 
 cd $installer_dir
 python3 -m http.server &
-server_pid=$!
 
 cd $docker_context
 installer_ip=`ifconfig docker0 | grep 'inet\s' | awk '{print $2}'`
@@ -15,7 +14,8 @@ docker build -t petalinux-docker:2014.4 \
              --build-arg installer_url=${installer_ip}:8000 \
              .
 
-kill $server_pid
 echo "---------------"
 echo "  Finish. ^_^  "
 echo "---------------"
+# Kill background job(s)
+kill -- -$$
